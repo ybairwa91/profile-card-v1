b465eac860cae850f9611115236a0c5958d3249a
Yogeshbairwa <ybairwa786@gmail.com>
2024-02-29 12:30:37 +0530

    completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles.css";
+import "./index.css";
+
+const sub = [
+  { name: "react", color: "blue" },
+  { name: "Html+css", color: "purple" },
+  { name: "Javascript", color: "red" },
+  { name: "Svelte", color: "pink" },
+];
 
 function App() {
   return (
@@ -8,18 +15,42 @@ function App() {
       <Avatar />
       <div className="data">
         <Intro />
-        {/* Should contain one Skill component
-        for each web dev skill that you have,
-        customized with props */}
         <SkillList />
       </div>
     </div>
   );
 }
 function Avatar() {
-  return <img src=""
+  return <img className="avatar" src="jonas.jpeg" alt="profile" />;
 }
-function Intro() {}
+function Intro() {
+  return (
+    <div>
+      <h1>Jonas Bairwa</h1>
+      <p>
+        I am passionate frontend developer known for design and attractive
+        websites,teaches student for skill enhancement.
+      </p>
+    </div>
+  );
+}
+function SkillList() {
+  return (
+    <div className="skill-list">
+      {sub.map((skill) => (
+        <Skill name={skill.name} color={skill.color} emoji="💪" />
+      ))}
+    </div>
+  );
+}
+
+function Skill({ name, color, emoji }) {
+  return (
+    <p className="skill" style={{ backgroundColor: color }}>
+      {name} {emoji}
+    </p>
+  );
+}
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
